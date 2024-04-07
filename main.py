71544fa85d61a59891ccca9887611009617a0881
Preston Chapman <pchapman82070@uat.edu>
2024-04-07 12:12:24 -0700

    Comments

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+# Impor the flask libary that allows for hosing a document on a local server
 from flask import Flask
 
+# This is a class for each skill, containing the name, level, and descrption
 class Skill:
     def __init__(self, name, level, desc):
         self.name = name
         self.level = level
         self.desc = desc
 
+# This is a list of all of the skill object
 all_skills = [
     Skill("Python", 1, "The basics of python"),
     Skill("Variables", 1, "Creating variables"),
@@ -18,30 +21,37 @@ all_skills = [
     Skill("Classes", 3, "Creating classes"),
     Skill("OOP", 4, "Object Oriented Programming"),
     ]
+# Create a webapp with the flask class using the name of the project
 web_app = Flask(__name__)
 
+# Set the default route
 @web_app.route("/")
+# Function for the home page
 def home_page():
-    document = ""
+    # Initialize the document string
+    document = "<br><br><h1>Preston C. Python Camp</h1><br>"
+    # Array of the opening information
     opening = [
-        "<br><br><h1>Preston C. Python Camp</h1>",
-        "<br>",
         "Welcome to Preston C. Python Camp",
         "<br> this camp is to help you understand how to code in Python,",
         "we will be going over many things such as functions, libarries, classes, object orentated programing, and much more!",
         "This 5 week class will help you get in the mindset of a coder, and help you improve.",
         "No matter your skill or experience this camp is for you, and will assist you no matter what!"
         ]
+    # For each line of the opening, append to the document
     for line in opening:
         document += line
+    # Create the skills section
     skills = "<br><br><h2>Skills</h2>"
+    # For each skill, appened the respective infomraiton to the skills section
     for skill in all_skills:
         skills += f"<h3>{skill.name}</h3>"
         skills += f"<br>Level: {skill.level}"
         skills += f"<br>Description: {skill.desc}"
+    # Return the document and skills section
     return document + skills
 
-
+# Run the web app
 if __name__ == "__main__":
     web_app.run()
     
